c111bb2748c14918977f2fb4960c16e81b25913b
Henry Rivera <henry.rivera@nyu.edu>
2020-10-26 19:00:37 -0400

    modifying code

--- trivia.py
+++ trivia.py
@@ -2,12 +2,14 @@ import random
 import json
 
 
+# Used to randomize order of questions AND order of choices
 def shuffle_in_place(original_list):
     for i in range(len(original_list)-1, 0, -1):
         pos = random.randint(0, i)
         original_list[i], original_list[pos] = original_list[pos], original_list[i]
 
 
+# Prints the question and it's choices and takes in user input to check against answer
 def ask_question(question, choices, ans):
     options = choices
     options.append(ans)
@@ -24,11 +26,18 @@ def ask_question(question, choices, ans):
         return -5
 
 
+# Takes in filename of file with JSON data, parses it, and returns data
+def parse_json(filename):
+    with open(filename) as f:
+        data = json.load(f)
+    return data
+
+
+# Plays one round of trivia and keeps track of round score
 def play_trivia():
     print("Let's Play A Round of Trivia!\n")
     score = 0
-    with open('Apprentice_TandemFor400_Data.json') as f:
-        data = json.load(f)
+    data = parse_json('Apprentice_TandemFor400_Data.json')
     shuffle_in_place(data)
     for i in range(10):
         points = ask_question(data[i]['question'], data[i]['incorrect'], data[i]['correct'])
@@ -36,6 +45,7 @@ def play_trivia():
     print("Your final score was:", score)
 
 
+# Begins one round of trivia and gives user ability to keep playing rounds
 def main():
     play = True
     play_trivia()
